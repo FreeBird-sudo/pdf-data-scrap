3f216f36a60556985fe1b2c9c34d1ab0d9248ca7
Free Bird <52630444+FreeBird-sudo@users.noreply.github.com>
2022-12-06 08:14:59 +0000

    json

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from PyPDF2 import PdfReader
+import json
 
 reader = PdfReader("table.pdf")
 number_of_pages = len(reader.pages)
 page = reader.pages[0]
 text = page.extract_text()
-print(text)
+# print(text)
+jsDic = json.dumps(text)
+jso = json.loads(jsDic)
+print(type(jso))
+# print(json_object)
